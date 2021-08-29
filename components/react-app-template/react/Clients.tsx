8e84811c2c3af4eabba93cdaf247d8aa20b6e2e0
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-08-29 17:28:45 -0300

    tela admin inicio

---
+++ components/react-app-template/react/Clients.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Layout, PageBlock } from "vtex.styleguide";
+
+async function Clients () {
+
+  return (
+    <Layout>
+    <PageBlock
+        title="Gerenciamento de clientes"
+        subtitle="Tela de gerenciamento e análise de clientes"
+        variation="full"
+    >
+      <div className="container">
+        
+      </div>
+    </PageBlock>
+  </Layout>
+  )
+}
+
+export default Clients
